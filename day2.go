df39da51c98c3821e0ee62b43f8ad252d52fa476
Caitlin-B <56935291+Caitlin-B@users.noreply.github.com>
2020-07-02 18:30:25 +0100

    completed day 2

--- day2.go
+++ day2.go
@@ -26,18 +26,38 @@ func main() {
 	for i, v := range opcodestr {
 		opcode[i], err = strconv.ParseInt(v, 10, 32)
 	}
-	fmt.Println(opcode)
+	//try all combinations of verb and noun until one produces required output
 
-	//loop through opcode + calc
-	for i := 0; i < len(opcode); i += 4 {
+	for i := 0; i < 100; i++ {
+		for j := 0; j < 100; j++ {
+			testOpcode := make([]int64, len(opcode))
+			copy(testOpcode, opcode)
+			testOpcode[1] = int64(i)
+			testOpcode[2] = int64(j)
+			output := calcOutput(testOpcode)
+			if output[0] == 19690720 {
+				fmt.Printf("Noun: %v\nVerb: %v\n", i, j)
+			}
+		}
+	}
+}
+
+func calcOutput(origOpcode []int64) []int64{
+	opcode := make([]int64, len(origOpcode))
+	copy(opcode, origOpcode)
+	for i := 0; i < len(opcode) - 3; i += 4 {
+		param1 := opcode[i + 1]
+		param2 := opcode[i + 2]
+		param3 := opcode[i + 3]
+		
 		if (opcode[i] == 99) {
 			i = len(opcode)
 		} else if (opcode[i] == 1) {
-			opcode[opcode[i + 3]] = opcode[opcode[i + 1]] + opcode[opcode[i + 2]]
+			opcode[param3] = opcode[param1] + opcode[param2]
 		} else if (opcode[i] == 2) {
-			opcode[opcode[i + 3]] = opcode[opcode[i + 1]] * opcode[opcode[i + 2]]
+			opcode[param3] = opcode[param1] * opcode[param2]
 		}
 	}
 
-	fmt.Println(opcode)
+	return opcode
 }
