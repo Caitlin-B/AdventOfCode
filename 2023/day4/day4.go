32e66c64300bbcf3859e4ecdc7ff32b5e0e40ec2
Caitlin-B <caitlinbishop94@hotmail.co.uk>
2024-01-24 11:46:41 +0000

    completed day 4

--- 2023/day4/day4.go
+++ 2023/day4/day4.go
@@ -6,9 +6,41 @@ import (
 	"strings"
 )
 
-const lenFirstSect = 5
-
 func main() {
+	//part1()
+	part2()
+}
+
+// slice of card matches
+// slice of card copies
+
+func part2() {
+	inputs := utils.ScanInput(4)
+
+	matchesOnCards := make([]int, len(inputs))
+	cardCopies := make([]int, len(inputs))
+	for i, card := range inputs {
+		matchesOnCards[i] = getNumberOfMatches(card)
+		cardCopies[i] = 1
+	}
+
+	for i, matches := range matchesOnCards {
+		for j := 0; j < cardCopies[i]; j++ {
+			for k := i + 1; k < i+1+matches; k++ {
+				cardCopies[k] = cardCopies[k] + 1
+			}
+		}
+	}
+
+	tot := 0
+	for _, n := range cardCopies {
+		tot += n
+	}
+
+	fmt.Println(tot)
+}
+
+func part1() {
 	inputs := utils.ScanInput(4)
 
 	sum := 0
@@ -17,23 +49,26 @@ func main() {
 		val := cardVal(winners, got)
 
 		sum += val
-		fmt.Println("cardval: ", val)
-		fmt.Println("-----------")
 	}
 	fmt.Println(sum)
 }
 
+func getNumberOfMatches(in string) int {
+	winners, got := getNums(in)
+	return cardMatches(winners, got)
+}
+
 func getNums(in string) (winners []string, got []string) {
 	arr := strings.Split(in, " ")
-	fmt.Println(arr, len(arr))
+	//fmt.Println(arr, len(arr))
 	arr = removeSpace(arr)
-	fmt.Println(arr, len(arr))
+	//fmt.Println(arr, len(arr))
 
 	pipe := indexOfStr(arr, "|")
 	winners = arr[2:pipe]
 	got = arr[pipe+1:]
-	fmt.Println(winners, got)
-	fmt.Println(len(winners), len(got))
+	//fmt.Println(winners, got)
+	//fmt.Println(len(winners), len(got))
 
 	return
 }
@@ -47,6 +82,15 @@ func cardVal(w, g []string) int {
 			} else {
 				count = count * 2
 			}
+		}
+	}
+	return count
+}
+func cardMatches(w, g []string) int {
+	count := 0
+	for _, n := range g {
+		if indexOfStr(w, n) != -1 {
+			count++
 		}
 	}
 	return count
