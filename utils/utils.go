f0682f0f792586a34188114eee07a4a365f3b110
Caitlin-B <caitlinbishop94@hotmail.co.uk>
2024-02-14 19:56:13 +0000

    wip

--- utils/utils.go
+++ utils/utils.go
@@ -1,36 +1,36 @@
 package utils
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-	"strconv"
+    "bufio"
+    "fmt"
+    "os"
+    "strconv"
 )
 
 func ScanInput(day int) []string {
-	file, err := os.Open(fmt.Sprintf("./2023/day%d/data.txt", day))
+    file, err := os.Open(fmt.Sprintf("./2023/day%d/data.txt", day))
 
-	if err != nil {
-		fmt.Println(err)
-	}
+    if err != nil {
+        fmt.Println(err)
+    }
 
-	scanner := bufio.NewScanner(file)
-	scanner.Split(bufio.ScanLines)
-	var lines []string
+    scanner := bufio.NewScanner(file)
+    scanner.Split(bufio.ScanLines)
+    var lines []string
 
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
+    for scanner.Scan() {
+        lines = append(lines, scanner.Text())
+    }
 
-	file.Close()
+    file.Close()
 
-	return lines
+    return lines
 }
 
 func GetNum(in string) int {
-	out, err := strconv.Atoi(in)
-	if err != nil {
-		fmt.Println(err)
-	}
-	return out
+    out, err := strconv.Atoi(in)
+    if err != nil {
+        fmt.Println(err)
+    }
+    return out
 }
