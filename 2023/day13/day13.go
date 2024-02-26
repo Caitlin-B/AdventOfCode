2c757cd664c56f633441be87331711d992882f72
Caitlin-B <caitlinbishop94@hotmail.co.uk>
2024-02-26 15:15:23 +0000

    day 13 part 2 wip

--- 2023/day13/day13.go
+++ 2023/day13/day13.go
@@ -7,6 +7,12 @@ import (
 
 func main() {
 	inputs := utils.ScanInput(13)
+	fmt.Println(day1(inputs))
+	fmt.Println(day2(inputs))
+
+}
+
+func day1(inputs []string) int {
 	tot := 0
 	pattern := make([]string, 0)
 	for _, in := range inputs {
@@ -24,8 +30,28 @@ func main() {
 		}
 		pattern = append(pattern, in)
 	}
+	return tot
+}
 
-	fmt.Println(tot)
+func day2(inputs []string) int {
+	tot := 0
+	pattern := make([]string, 0)
+	for _, in := range inputs {
+		if in == "" {
+			if r := getRowReflectionSmudged(pattern); r != 0 {
+				tot += r * 100
+				pattern = []string{}
+				continue
+			}
+			refl := swapRowCol(pattern)
+			c := getRowReflectionSmudged(refl)
+			tot += c
+			pattern = []string{}
+			continue
+		}
+		pattern = append(pattern, in)
+	}
+	return tot
 }
 
 // getRowReflection - returns 0 if no horizontal reflection
@@ -71,4 +97,58 @@ func swapRowCol(in []string) []string {
 		swapped = append(swapped, col)
 	}
 	return swapped
+}
+
+// isOneCharDifferent compares two rows and returns true if they are the same bar one character
+func isOneCharDifferent(a, b string) bool {
+	diffs := 0
+	for i := range a {
+		if a[i] != b[i] {
+			diffs++
+		}
+	}
+
+	return diffs == 1
+}
+
+// isRowReflectionSmudged does the same as isRowReflection but allows for one smudged row where the rows are the same in all but one character
+func isRowReflectionSmudged(a, b []string, smudged bool) bool {
+	a2 := make([]string, len(a))
+	copy(a2, a)
+	for i, j := 0, len(a2)-1; i < j; i, j = i+1, j-1 {
+		a2[i], a2[j] = a2[j], a2[i]
+	}
+	length := len(a2)
+	if len(b) < length {
+		length = len(b)
+	}
+
+	for i := 1; i < length; i++ { // already know first rows are a match
+		if a2[i] != b[i] {
+			if !smudged && isOneCharDifferent(a2[i], b[i]) {
+				smudged = true
+				continue
+			}
+			return false
+		}
+	}
+	return true
+}
+
+// getRowReflectionSmudged - returns 0 if no horizontal reflection
+func getRowReflectionSmudged(in []string) int {
+	for i, r := range in {
+		if i == 0 {
+			continue
+		}
+		oneOff := isOneCharDifferent(r, in[i-1])
+		if r == in[i-1] || oneOff { // if row is same as row before
+			firstHalf := in[:i]
+			secondHalf := in[i:]
+			if isRowReflectionSmudged(firstHalf, secondHalf, oneOff) {
+				return i
+			}
+		}
+	}
+	return 0
 }
